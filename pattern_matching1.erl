7a37e9694f4a255a7b33ea62f97b0f0cf1771d30
alex chan <chan.k.leong@gmail.com>
2017-03-06 08:12:26 +0800

    Added pattern_matching1.erl

---
+++ pattern_matching1.erl
@@ -0,0 +1,37 @@
+
+- module(pattern_matching1).
+
+- compile(export_all).
+
+%% Because of pattern matching, there is no if...else... block in erlang.
+
+
+
+say_hello(male, Name) ->
+  io:format("Hello, Mr ~s! ~n", [Name]);  %% Note the ";" at the end.
+  
+say_hello(female, Name) ->
+  io:format("Hello, Ms ~s! ~n", [Name]);  %% Note the ";"
+
+say_hello(_, Name) ->                     %% This is necessarily.
+  io:format("Hello, ~s! ~n", [Name]).     %% Note the "."
+
+
+%% The above is the equivalent of something like:
+%% 
+%% function say_hello(gender, Name):
+%%
+%%  if gender == male
+%%     print "...."
+%%
+%%  else if gender == female
+%%     print "...."
+%% 
+%%  else
+%%     print
+
+
+%% Function clauses are seperated by ";"
+%% final function ends with "."
+
+
