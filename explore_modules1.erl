621ba64c1b6ea03132e4eeab8f01a43f444f55ea
alex chan <chan.k.leong@gmail.com>
2017-03-07 22:00:02 +0800

    added stuff to explore_modules1.erl

--- explore_modules1.erl
+++ explore_modules1.erl
@@ -3,6 +3,9 @@
 
 - export([add1/1, add2/1, add3/1]).
 - export([add/1, add/2]).
+- export([circle_area/1]).
+- export([add1_v2/1]).
+
 
 add1(X) ->
   X + 1.
@@ -27,3 +30,40 @@ add(X, Y) ->
 %% 10
 %% 25> explore_modules1:add(10, 11).
 %% 21
+
+%% Macros
+
+%% - define() is used to defined macros.
+%% Macros can be used anywhere within the module.
+%% To refer to an macro, prefix with a "?"
+
+- define(Pi,3.14).  % imprecise PI
+
+circle_area(R) ->
+  ?Pi * R * R.
+
+
+%% It is possible to define a "function" macro.
+
+- define(plus_one(X), X + 1).
+
+add1_v2(X) -> ?plus_one(X).
+
+
+%% It is posible to check if a particular macro has been defined in the module with
+%% - ifdef()
+
+
+%% The below function will be compiled only if the TEST macro was defined.
+%% Can be defined via compiler options e.g:
+%%
+%%   c(explore_modules1, [{d, 'TEST'}]).
+%% 
+
+- ifdef(TEST).
+
+test1() ->
+  io:format("TEST macro is defined. ~n").
+
+- endif.
+
