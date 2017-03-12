91311d663da00e37f009da0f8ad35ca5134f21cc
alex chan <chan.k.leong@gmail.com>
2017-03-12 11:09:58 +0800

    add stuff to recursive1.erl

--- recursive1.erl
+++ recursive1.erl
@@ -4,8 +4,20 @@
 
 
 
-length1([]) -> 0.  % this is require to match empty list case.
+length1([]) -> 0;     % this is require to match empty list case.
 length1([_|T]) -> 1 + length(T).
 
-% 7> recursive1:length1([1,2,3,a,5,g]).
-% 6
+%% 7> recursive1:length1([1,2,3,a,5,g]).
+%% 6
+
+%% The above recursion is not tail recursive. A list with a million values
+%% will use a lot of memory.
+
+%% To make it tail recursive, need to use accumulator.
+
+
+
+tail_length1(L) -> tail_length1(L,0).
+
+tail_length1([],N) -> N;
+tail_length1([_|T],N) -> tail_length1(T,N+1).
